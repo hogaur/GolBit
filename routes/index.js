de5a3bf54b3efbaa7ca5ef8a9f85261c8b9034df
hogaur <hogaur@gmail.com>
2014-05-21 03:17:52 +0530

    first commit

---
+++ routes/index.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+// router.get('/', function(req, res) {
+//   res.render('index', { title: 'Express' });
+// });
+
+
+router.get('/', function(req, res) {
+    res.render('index',{title: 'GolBit',});
+});
+
+ 
+router.get('/about', function(req, res) {
+    res.render('about',{title: 'About'});
+});
+ 
+router.get('/article', function(req, res) {
+  var db=req.db;
+  var collection= db.get('blogcollection');
+  collection.find({},{},function(e,docs){
+    res.render('blogs',{
+      "articlelist":docs
+    });
+  });
+});
+
+router.get('/:id', function(req, res) {
+  var db=req.db;
+  var collection= db.get('blogcollection');
+  collection.find({_id: collection.ObjectId(req.params.id) }, {},function(e,docs){
+    content = docs;
+  });
+    res.render('article',{
+      article:docs
+    });
+  });
+
+module.exports = router;
